4b1bf5ddb3eb23138ea77ae68f43ebe1e791c117
Matthew Beall <matthew@beall.org>
2023-04-26 15:29:57 -0600

    add more projects

---
+++ dbt-1.0/bigquery/large_project_40k_models/models/staging/tpch_data/stg__tpch_data_nation.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from {{ source('tpch_data', 'nation') }}
+
+),
+
+renamed as (
+
+    select
+        n_nationkey,
+        n_name,
+        n_regionkey,
+        n_comment
+
+    from source
+
+)
+
+select * from renamed
+
---
+++ dbt-1.0/bigquery/large_project_40k_models/models/staging/tpch_data/stg__tpch_data_region.sql
@@ -0,0 +1,19 @@
+with source as (
+
+    select * from {{ source('tpch_data', 'region') }}
+
+),
+
+renamed as (
+
+    select
+        r_regionkey,
+        r_name,
+        r_comment
+
+    from source
+
+)
+
+select * from renamed
+
